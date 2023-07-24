ee34da50f05b3fe78e0ad2b5e895a611c28defda
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-07-24 12:50:49 -0400

    AM Algo

--- Algorithm/wk03d1/basic13practice.js
+++ Algorithm/wk03d1/basic13practice.js
@@ -19,9 +19,9 @@ function print255() {
 
 function printsum255() {
     var sum = 0;
-    for(var i = 0; i < 256; i++) {
+    for (var i = 0; i < 256; i++) {
         sum = sum + i;
-        console.log(sum);
+        console.log("current number is " + i, "Current Sum is " + sum);
     }
 }
 
@@ -32,12 +32,12 @@ function printsum255() {
 // Find and Print Max
 // Given an array, find and print its largest element.
 
-arr = [13,2,3,4,5,8,9];
+arr = [13, 2, 3, 4, 5, 8, 9];
 
 function findMax() {
     max = arr[0];
-    for(var i = 1; i < arr.length; i++)
-        if(arr[i] > max){
+    for (var i = 1; i < arr.length; i++)
+        if (arr[i] > max) {
             max = arr[i];
         }
     console.log(max);
@@ -52,18 +52,32 @@ function findMax() {
 
 function oddArray255() {
     var oddarr = []
-    for(var i = 0; i < 256; i++){
-        if(i % 2 !== 0){
+    for (var i = 0; i < 256; i++) {
+        if (i % 2 !== 0) {
             oddarr.push(i);
         }
     }
-    console.log(oddarr);    }
+    console.log(oddarr);
+}
 
-oddArray255();
+// oddArray255();
+
 // ############################################################################
 
 // Greater Than Y
 // Given an array and a value Y, count and print the number of array values greater than Y.
+arr = [13, 2, 3, 4,]
+y = 4
+
+function greaterthanY() {
+    for (var i = 0; i < arr.length; i++) {
+        if (y > arr[i]) {
+            console.log(arr[i]);
+        }
+    }
+}
+
+// greaterthanY()
 
 
 // ############################################################################
@@ -71,39 +85,108 @@ oddArray255();
 // Max, Min, Average
 // Given an array, print the max, min and average values for that array.
 
+arr = [1, 2, 3, 4]
+max = arr[0];
+min = arr[0];
+sum = 0;
+
+function maxMinAvg() {
+    for (var i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+        sum += arr[i];
+    }
+    console.log("Max is " + max, "Min is " + min, "Avg is " + sum/arr.length);
+}
+
+// maxMinAvg()
 
 // ############################################################################
 
 // Swap String For Array Negative Values
 // Replace any negative array values with 'Dojo'.
 
+function swapStringFroArrayNeg(arr){
+    for( var i =0; i< arr.length; i++){
+        if(arr[i] < 0) {
+            arr[i] = "Dojo"
+        }
+    }
+    console.log(arr)
+}
 
+// swapStringFroArrayNeg([-1,-10,1,2,3,4,5])
 
 // ############################################################################
 
 // Print Odds 1-255
 // Print all odd integers from 1 to 255.
 
+function printOdds1to255() {
+    for( var i = 1; i< 256; i++){
+        if (i%2 != 0){
+            console.log(i + " is odd.")
+        }
+    }
+}
+
+// printOdds1to255()
+
 // ############################################################################
 
 // Iterate and Print Array
 // Iterate through a given array, printing each value.
+
+function printArray(arr) {
+    for(var i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+}
+
+// printArray([1,2,3,4,5,6])
 
 // ############################################################################
 
 // Get and Print Average
 // Analyze an array’s values and print the average.
 
+function printAvg(arr){
+    var sum = 0;
+    for ( var i = 0; i< arr.length; i++){
+        sum += arr[i];  
+    }
+    console.log(sum/arr.length);
+}
+
+// printAvg([2,4])
+
 // ############################################################################
 
 // Square the Values
 // Square each value in a given array, returning that same array with changed values.
 
+arr = [1,2,3,4,5,6,7,]
+
+function squareValue() {
+    for (var i = 0; i < arr.length; i++)   {
+        squaredValue = arr[i]**2;
+        arr[i] = squaredValue;
+    }
+    console.log(arr);
+}
+
+squareValue()
 
 // ############################################################################
 
 // Zero Out Negative Numbers
 // Return the given array, after setting any negative values to zero.
+
+
 
 // ############################################################################
 
